1a75e062aea1cec61630a5ba225c4b9c9909e924
TG2408 <tanmaygupta2408@gmail.com>
2021-11-24 15:10:56 +0530

    database schema ready

--- routes/aliens.js
+++ routes/aliens.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("get request")
+router.get('/', async(req, res) => {
+    try {
+
+    } catch (err) {
+        res.send("Error: " + err);
+    }
 });
 
 module.exports = router;
