03029e3e06c2a750f620dfa9fedd9780f1415042
TG2408 <tanmaygupta2408@gmail.com>
2021-11-24 14:29:39 +0530

    get route added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const url = "mongodb://localhost/AlienDBex";
 
 const app = express();
@@ -10,3 +11,11 @@ const con = mongoose.connection;
 con.on("open", () => {
     console.log("connected....");
 })
+
+const alienRouter = require('./routes/aliens');
+app.use('/aliens', alienRouter);
+
+
+app.listen(9000, () => {
+    console.log("server started");
+});
