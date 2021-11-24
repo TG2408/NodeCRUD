3f2d2549680749e3517f01c81f91f491e0754e81
TG2408 <tanmaygupta2408@gmail.com>
2021-11-24 15:53:26 +0530

    get/ get by id/ post/ patch/ delete request working

--- routes/aliens.js
+++ routes/aliens.js
@@ -47,4 +47,14 @@ router.patch('/:id', async(req, res) => {
     }
 })
 
+router.delete('/:id', async(req, res) => {
+    try {
+        const alien = await Alien.findById(req.params.id);
+        const a1 = await alien.remove();
+        res.json(a1);
+    } catch(err) {
+        res.send(err);
+    }
+})
+
 module.exports = router;
